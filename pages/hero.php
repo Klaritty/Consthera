9c0595e56e02114263211359311a32240b466bfb
lucas <lucas1995giorgi@gmail.com>
2024-05-23 17:29:54 -0300

    fixes

--- pages/hero.php
+++ pages/hero.php
@@ -1,33 +1,33 @@
-<section id='inicio' class='bg-cover bg-no-repeat bg-center'>
+<?php
+
+
+?>
+<section id='inicio' class=' hidden md:block bg-cover bg-no-repeat bg-center' style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/assets/images/home_hero_back_desk.jpg');">
     <div class='pb-[6rem] pt-[6rem] container mx-auto'>
-        <img class='w-[10rem] h-[10rem] mb-[3rem] mx-auto' alt='consthera' src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/consthera_star.svg"/>
-        <h1 class='text-center pb-[2rem] font-belleza text-[40px] mp:text-[60px] mx-auto w-[90%] mp:w-[70%] text-white'>
-            Encuentra la sanación integral, el bienestar y la paz interior en Consthera.
-        </h1>
-        <div class="w-[10%] mx-auto border-[#BD9062] border-t-2 mb-[2rem]"></div>
+        <img class='w-[10rem] h-[10rem] mb-[3rem] mx-auto' alt='cosnthera' src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/consthera_star.svg"/>
+        <h1 class='text-center pb-[2rem] font-belleza text-[40px]  mp:text-[60px] mx-auto w-[90%] mp:w-[70%] text-white'>Encuentra la sanación integral, el bienestar y la paz interior en Consthera.</h1>
+        <div class=" w-[10%] mx-auto border-[#BD9062] border-t-2 mb-[2rem]"></div>
         <div class='flex justify-center items-center'>
-            <p class='text-center mx-auto font-rosario text-[23px] w-[80%] mp:w-[60%] text-white'>
-                Bienvenido a un espacio de tranquilidad y crecimiento, donde te ayudamos a sanar, comprenderte y sentirte en armonía contigo mismo.
+            <p class='text-center mx-auto font-rosario text-[23px] w-[80%] mp:w-[60%] text-white'>Bienvenido a un espacio de tranquilidad y crecimiento, donde te ayudamos a sanar, comprenderte y sentirte en armonía contigo mismo.
             </p>
         </div>
         <div class="buttons-services mt-[3rem]">
-            <a href="https://wa.me/528141485521" class='font-rosario bg-[#BD9062] no-underline py-2 px-5 rounded-full text-white text-[14px]'>RESERVA UNA SESIÓN</a>
-        </div>
+        <a  href="https://wa.me/528141485521" class='font-rosario bg-[#BD9062] no-underline py-2 px-5 rounded-full text-white text-[14px]'>RESERVA UNA SESIÓN</a>
+      </div>
     </div>
 </section>
 
-<style>
-    @media (min-width: 768px) {
-        #inicio {
-            background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/assets/images/home_hero_back_desk.jpg');
-        }
-    }
-
-    @media (max-width: 767px) {
-        #inicio {
-            background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/assets/images/home_hero_back_mob.jpg');
-        }
-    }
-</style>
-
-<script src="script.js" defer></script>
+<section id='inicio' class='md:hidden bg-cover bg-no-repeat bg-center' style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/assets/images/home_hero_back_mob.jpg');">
+    <div class='pb-[6rem] pt-[6rem] container mx-auto'>
+        <img class='w-[10rem] h-[10rem] mb-[3rem] mx-auto' alt='cosnthera' src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/consthera_star.svg"/>
+        <h1 class='text-center pb-[2rem] font-belleza text-[40px]  mp:text-[60px] mx-auto w-[90%] mp:w-[70%] text-white'>Encuentra la sanación integral, el bienestar y la paz interior en Consthera.</h1>
+        <div class=" w-[10%] mx-auto border-[#BD9062] border-t-2 mb-[2rem]"></div>
+        <div class='flex justify-center items-center'>
+            <p class='text-center mx-auto font-rosario text-[23px] w-[80%] mp:w-[60%] text-white'>Bienvenido a un espacio de tranquilidad y crecimiento, donde te ayudamos a sanar, comprenderte y sentirte en armonía contigo mismo.
+            </p>
+        </div>
+        <div class="buttons-services mt-[3rem]">
+        <a  href="https://wa.me/528141485521" class='font-rosario bg-[#BD9062] no-underline py-2 px-5 rounded-full text-white text-[14px]'>RESERVA UNA SESIÓN</a>
+      </div>
+    </div>
+</section>
