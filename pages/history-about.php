5012a8f76aea1d8f668204bfd0434707a8a66149
lucas <lucas1995giorgi@gmail.com>
2024-05-27 19:12:06 -0300

    change pharagraph

--- pages/history-about.php
+++ pages/history-about.php
@@ -7,12 +7,23 @@
         <h1 class='text-center pb-[2rem] font-belleza text-[60px] text-[#0A2A45] '>HISTORIA</h1>
         <div class=" w-[10%] mx-auto border-[#BD9062] border-t-2 mb-[2rem]"></div>
         <div class='pb-[4rem] flex flex-col items-center'>
-            <p class='text-center font-rosario w-[60%] text-[#0A2A45] '>Consthera surge de un viaje personal de autodescubrimiento y transformación que abarca generaciones. Hace 4 años, en el último tramo de su diplomado en Constelaciones Familiares nuestra fundadora, Alma, se dio cuenta de que su propósito trascendía la psicología convencional.
+            <p class='text-center font-rosario w-[60%] text-[#0A2A45] '>Consthera surge de un viaje personal de
+                autodescubrimiento y transformación que abarca generaciones. Hace 4 años, en el último tramo de su
+                diplomado en Constelaciones Familiares nuestra fundadora, Alma, se dio cuenta de que su propósito
+                trascendía la psicología convencional.
             </p>
             <br>
-            <p class='text-center font-rosario w-[60%] text-[#0A2A45] '>Su padre desaprobó la idea de que ella estudiara psicología, esto fue el punto de inflexión y el inicio a la experiencia de Constelaciones Familiares. No solo fue un despertar para ella, sino también para su legado ancestral y comunidad.</p><br>
-            <p class='text-center font-rosario w-[60%] text-[#0A2A45] '>Alma había experimentado, durante años, visiones y mensajes de seres queridos fallecidos y entidades de otras dimensiones, sin comprender completamente su significado o propósito. </p><br>
-            <p class='text-center font-rosario w-[60%] text-[#0A2A45] '>En una reunión con amigos y la repentina presencia de un familiar fallecido de ellos, reconoció el valor de su don, potencial y propósito para ayudar a otros. Al transmitir mensajes de personas que murieron y trascendieron, se dio cuenta del poder de la comunicación entre los dos mundos. Esto la inspiró a explorar más a fondo su conexión con el mundo espiritual y a desarrollar las terapias integrales que hoy ofrecemos en Consthera. </p>
+            <p class='text-center font-rosario w-[60%] text-[#0A2A45] '>Su padre desaprobó la idea de que ella estudiara
+                psicología, esto fue el punto de inflexión y el inicio a la experiencia de Constelaciones Familiares. No
+                solo fue un despertar para ella, sino también para su legado ancestral y comunidad.</p><br>
+            <p class='text-center font-rosario w-[60%] text-[#0A2A45] '>Alma había experimentado, durante años, visiones
+                y mensajes de seres queridos fallecidos y entidades de otras dimensiones, sin comprender completamente
+                su significado o propósito. </p><br>
+            <p class='text-center font-rosario w-[60%] text-[#0A2A45] '>En una reunión con amigos y la repentina
+                presencia de un familiar fallecido de ellos, reconoció el valor de su don, potencial y propósito para
+                ayudar a otros. Al transmitir mensajes de personas que murieron y trascendieron, se dio cuenta del poder
+                de la comunicación entre los dos mundos. Esto la inspiró a explorar más a fondo su conexión con el mundo
+                espiritual y a desarrollar las terapias integrales que hoy ofrecemos en Consthera. </p>
         </div>
 </section>
 <section class='bg-[#e8eaee] pb-[3rem] pt-[3rem]'>
@@ -21,8 +32,8 @@
     <div class="lg:card flex flex-col justify-center md:flex-col items-center md:py-0 lg:py-0">
         <div class=' mt-[3rem] mb-[2rem]'>
             <div class='mt-[2rem] md:mt-0 flex items-center justify-center'>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/circle_frame.svg" alt="PSIC. ALMA SOLÍS"
-                    class='object-cover  w-[65%] md:w-[22%] absolute ' />
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/circle_frame.svg"
+                    alt="PSIC. ALMA SOLÍS" class='object-cover  w-[65%] md:w-[22%] absolute ' />
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/about_alma.jpg"
                     alt="PSIC. ALMA SOLÍS" class='w-[60%] h-auto rounded-full relative ' />
             </div>
@@ -78,9 +89,9 @@
                 </h2>
                 <h3 class="text-[#0A2A45]  font-rosario text-[20px] ">Instituto de Constelaciones Familiares de
                     Monterrey (2021)</h3><br>
-                <p class="text-[#0A2A45] w-[70%] mx-auto font-rosario text-[16px] ">Abarcar sesiones grupales,
-                    individuales y de pareja. Identificar dinámicas familiares que obstaculizan relaciones
-                    presentes, desentrañar patrones disfuncionales y guiar hacia soluciones al acceder al
+                <p class="text-[#0A2A45] w-[70%] mx-auto font-rosario text-[16px] ">Con un enfoque en dinámicas
+                    familiares, esta especialidad ayuda a identificar en el presente, tanto en sesiones grupales como
+                    individuales y de pareja, los patrones disfuncionales y guiar hacia soluciones mediante el acceso al
                     inconsciente para sanar y liberar emociones bloqueadas.</p>
             </div>
         </div>
@@ -93,10 +104,13 @@
                     Canalización con Guías y seres Espirituales
                 </h2>
                 <h3 class="text-[#0A2A45]  font-rosario text-[20px] ">Escuela Sol Ahimsa (2024)</h3><br>
-                <p class="text-[#0A2A45] w-[70%] mx-auto font-rosario text-[16px] ">Dominar la canalización con
-                    guías y seres espirituales. Identificar mensajes auténticos y expandir la conciencia hacia
-                    dimensiones superiores, reduciendo el ruido mental y conectando con la luz interior para
-                    ayudar a otros.</p>
+                <p class="text-[#0A2A45] w-[70%] mx-auto font-rosario text-[16px] ">Como canalizadora espiritual
+                    identificas mensajes auténticos para expandir la conciencia hacia dimensiones superiores que
+                    disminuyen el ruido mental y conectan con la luz interior para ayudar a otros.
+
+                    Así como mantener el estado de trance, bloqueando los propios pensamientos y solicitar la conexión
+                    con el guía.
+                </p>
             </div>
         </div>
     </div>
@@ -107,12 +121,9 @@
                 <h2 class="text-[#BD9062]  font-belleza  text-[30px]  mb-2 lg:mt-[1rem] lg:mb-[1rem]">
                     Limpieza Energética y Autodefensa Psíquica
                 </h2>
-                <h3 class="text-[#0A2A45]  font-rosario text-[20px] "> Paduka con la Astrologia Paola Michel(2024)</h3><br>
-                <p class="text-[#0A2A45] w-[70%] mx-auto font-rosario text-[16px] ">Aprender técnicas de limpieza
-                    energética
-                    y autodefensa psíquica. Desde fortalecer el sistema energético hasta conectar con la luz
-                    interior, explorar visualizaciones guiadas y la generación de energía renovada para
-                    equilibrar nuestro campo aurico.</p>
+                <h3 class="text-[#0A2A45]  font-rosario text-[20px] "> Paduka con la Astrologia Paola Michel(2024)</h3>
+                <br>
+                <p class="text-[#0A2A45] w-[70%] mx-auto font-rosario text-[16px] ">Esta formación permite fortalecer el sistema energético y conectar con la luz interior. Explora técnicas de visualización guiada y generación de energía renovada para equilibrar el campo áurico, junto con técnicas de limpieza energética y autodefensa psíquica.</p>
             </div>
         </div>
     </div>
