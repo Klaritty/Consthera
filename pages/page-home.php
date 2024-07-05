88300c81eaff6a821183f1d2b40ab6b5d06ad58e
lucas <lucas1995giorgi@gmail.com>
2024-07-05 16:52:37 -0300

    reserva

--- pages/page-home.php
+++ pages/page-home.php
@@ -60,7 +60,7 @@ Template Name: Home
         <!--Testimonios-->
         <?php include 'testimonials.php'?>
         <!--Reservation-->
-        <?php include 'history-abouut.php'?>
+        <?php include 'history-about.php'?>
         <!--contacto-->
         <?php include 'contacto.php'?>
         <!--Footer-->
